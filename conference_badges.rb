cb0c8d93e1d170a7195125635184457137638822
Gilbert Torchon <gilbert@noukod.com>
2018-09-17 19:57:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,12 @@
 # Write your code here.
-def batch_bade_creator(array)
+def batch_badge_creator(array)
   array.each do |member|
-    puts "Hello, my name is #{member}."
+    badge_maker(member)
   end
 end
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}"
+end
+
+
